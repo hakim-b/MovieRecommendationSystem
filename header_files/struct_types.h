1bc8609dcdd08b19b809734224f6e5e8b7d406a4
hakim-b <hakimbouabdellah02@gmail.com>
2024-02-08 23:52:42 -0500

    first commit

---
+++ header_files/struct_types.h
@@ -0,0 +1,17 @@
+//
+// Created by hakim on 2/8/24.
+//
+
+#ifndef MOVIERECCOMENDATIONSYSTEM_STRUCT_TYPES_H
+#define MOVIERECCOMENDATIONSYSTEM_STRUCT_TYPES_H
+
+struct User {
+    char username[20];
+};
+
+struct Movie {
+    char title[30];
+    int rating
+};
+
+#endif //MOVIERECCOMENDATIONSYSTEM_STRUCT_TYPES_H
---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+void printMenu();
+
+
+int main() {
+    return 0;
+}
+
+void printMenu() {
+    printf("***** Movie Recommendation System *****\n");
+    printf("1. Register User\n");
+    printf("2. Display Movies\n");
+    printf("3. Rate a Movie\n");
+    printf("4. Get Movie Recommendations\n");
+    printf("0. Exit\n");
+}
